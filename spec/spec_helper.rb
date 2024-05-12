6aab8f89931a099c9778a500de1b74737442a3b2
Maksymilian Orzechowski <morzechowski8@gmail.com>
2024-05-12 18:51:17 +0200

    moved the mongoid rspec definition

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-
-require 'mongoid-rspec'
-
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
