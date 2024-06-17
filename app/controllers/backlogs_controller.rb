94af250d6260f505a2f39a1c07a6adb74e00264f
Maksymilian Orzechowski <morzechowski8@gmail.com>
2024-06-17 22:59:40 +0200

    create action for Backlog

--- app/controllers/backlogs_controller.rb
+++ app/controllers/backlogs_controller.rb
@@ -4,4 +4,18 @@ class BacklogsController < ApplicationController
   def index
     render json: Backlog.all
   end
+
+  def create
+    backlog = Backlog.create!(backlog_params)
+    if backlog
+      render json: backlog
+    else
+      render json: backlog.errors
+    end
+  end
+
+  private
+  def backlog_params
+    params.permit(:name, :type, :content, :due_at)
+  end
 end
--- spec/controllers/backlogs_controller_spec.rb
+++ spec/controllers/backlogs_controller_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe BacklogsController, type: :controller do
       expect(JSON.parse(response.body).length).to eq 10
     end
   end
+
+  describe 'POST create' do
+    let(:params) { { name: "Example backlog", type: 'books', content: [ { title: "Title", score: 7, status: 'completed' },
+                   { title: "Title", status: 'to do' } ], due_at: 1.month.from_now } }
+    it 'creates a new backlog' do
+      expect do
+        post :create, params: params
+      end.to change{ Backlog.count }.by 1
+    end
+
+    it 'returns successful response' do
+      post :create, params: params
+      expect(response).to be_successful
+      expect(JSON.parse(response.body)["name"]).to eq 'Example backlog'
+    end
+  end
 end
