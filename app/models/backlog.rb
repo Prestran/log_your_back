674233f03eeadab00ac5d18c8c6cbde1f9e26500
Maksymilian Orzechowski <morzechowski8@gmail.com>
2025-03-10 19:21:29 +0100

    turbo streams for backlog

--- app/models/backlog.rb
+++ app/models/backlog.rb
@@ -6,4 +6,9 @@ class Backlog < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit -> { broadcast_prepend_later_to 'backlogs' }
+  after_update_commit -> { broadcast_replace_later_to 'backlogs' }
+  after_destroy_commit -> { broadcast_remove_to 'backlogs' }
+  # broadcasts_to ->(backlog) { "backlogs" }, inserts_by: :prepend
 end
